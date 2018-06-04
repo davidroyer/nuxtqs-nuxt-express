cbdf4f1b5e5051765fb0cf1fb19a49d04e885c3f
David Royer <droyer01@gmail.com>
2018-06-04 08:47:17 -0400

    moved now config back to package.json

--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -8,7 +8,7 @@
       <h2 class="subtitle">
         Using Custom Directory Structure
       </h2>
-      <h3>Something New 2</h3>
+      <h3>Using package.json for now config</h3>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
